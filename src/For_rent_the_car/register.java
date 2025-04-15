5a8d784e3d4b29c894c95df1f508cd2e5381ddbe
Shin2930 <visud4545@gmail.com>
2025-04-15 17:04:11 +0700

    Add files via upload

--- src/For_rent_the_car/register.java
+++ src/For_rent_the_car/register.java
@@ -8,12 +8,12 @@ package For_rent_the_car;
  *
  * @author visud
  */
-public class login extends javax.swing.JFrame {
+public class register extends javax.swing.JFrame {
 
     /**
      * Creates new form NewJFrame
      */
-    public login() {
+    public register() {
         initComponents();
     }
 
@@ -27,27 +27,26 @@ public class login extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        txt_login = new javax.swing.JLabel();
+        txt_register = new javax.swing.JLabel();
         icon_user = new javax.swing.JLabel();
         icon_password = new javax.swing.JLabel();
         txt_user = new javax.swing.JTextField();
         txt_password = new javax.swing.JTextField();
         bt_reset = new javax.swing.JButton();
-        bt_login = new javax.swing.JButton();
+        bt_Submit = new javax.swing.JButton();
         icon_close = new javax.swing.JLabel();
-        jButton1 = new javax.swing.JButton();
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setBackground(new java.awt.Color(0, 255, 255));
 
-        jPanel1.setBackground(new java.awt.Color(0, 255, 255));
+        jPanel1.setBackground(new java.awt.Color(204, 255, 255));
         jPanel1.setToolTipText("");
 
-        txt_login.setBackground(new java.awt.Color(51, 255, 0));
-        txt_login.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
-        txt_login.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        txt_login.setText("Login");
-        txt_login.setAutoscrolls(true);
+        txt_register.setBackground(new java.awt.Color(51, 255, 0));
+        txt_register.setFont(new java.awt.Font("Segoe UI", 1, 36)); // NOI18N
+        txt_register.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        txt_register.setText("Register");
+        txt_register.setAutoscrolls(true);
 
         icon_user.setIcon(new javax.swing.ImageIcon(getClass().getResource("/For_rent_the_car/user-2-24.png"))); // NOI18N
 
@@ -57,14 +56,11 @@ public class login extends javax.swing.JFrame {
         bt_reset.setIcon(new javax.swing.ImageIcon(getClass().getResource("/For_rent_the_car/undo-2-24.png"))); // NOI18N
         bt_reset.setText("Reset");
 
-        bt_login.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
-        bt_login.setIcon(new javax.swing.ImageIcon(getClass().getResource("/For_rent_the_car/login-24.png"))); // NOI18N
-        bt_login.setText("Login");
+        bt_Submit.setFont(new java.awt.Font("Segoe UI", 1, 18)); // NOI18N
+        bt_Submit.setIcon(new javax.swing.ImageIcon(getClass().getResource("/For_rent_the_car/login-24.png"))); // NOI18N
+        bt_Submit.setText("Submit");
 
         icon_close.setIcon(new javax.swing.ImageIcon(getClass().getResource("/For_rent_the_car/close-window-24.png"))); // NOI18N
-
-        jButton1.setFont(new java.awt.Font("Segoe UI", 1, 14)); // NOI18N
-        jButton1.setText("Register");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -80,19 +76,17 @@ public class login extends javax.swing.JFrame {
                     .addGroup(jPanel1Layout.createSequentialGroup()
                         .addComponent(bt_reset)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 30, Short.MAX_VALUE)
-                        .addComponent(bt_login))
+                        .addComponent(bt_Submit))
                     .addComponent(txt_user)
                     .addComponent(txt_password))
-                .addContainerGap(75, Short.MAX_VALUE))
+                .addContainerGap(62, Short.MAX_VALUE))
             .addGroup(jPanel1Layout.createSequentialGroup()
                 .addContainerGap()
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                    .addComponent(txt_login, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                    .addComponent(txt_register, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                     .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                        .addGap(0, 0, Short.MAX_VALUE)
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(icon_close, javax.swing.GroupLayout.Alignment.TRAILING)
-                            .addComponent(jButton1, javax.swing.GroupLayout.Alignment.TRAILING))))
+                        .addGap(0, 360, Short.MAX_VALUE)
+                        .addComponent(icon_close)))
                 .addContainerGap())
         );
         jPanel1Layout.setVerticalGroup(
@@ -101,7 +95,7 @@ public class login extends javax.swing.JFrame {
                 .addGap(10, 10, 10)
                 .addComponent(icon_close)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
-                .addComponent(txt_login)
+                .addComponent(txt_register)
                 .addGap(33, 33, 33)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
                     .addComponent(icon_user)
@@ -113,10 +107,8 @@ public class login extends javax.swing.JFrame {
                 .addGap(40, 40, 40)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
                     .addComponent(bt_reset, javax.swing.GroupLayout.DEFAULT_SIZE, 38, Short.MAX_VALUE)
-                    .addComponent(bt_login, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-                .addGap(18, 18, 18)
-                .addComponent(jButton1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                .addContainerGap())
+                    .addComponent(bt_Submit, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(51, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -150,35 +142,40 @@ public class login extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(login.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(register.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new login().setVisible(true);
+                new register().setVisible(true);
             }
         });
     }
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JButton bt_login;
+    private javax.swing.JButton bt_Submit;
     private javax.swing.JButton bt_reset;
     private javax.swing.JLabel icon_close;
     private javax.swing.JLabel icon_password;
     private javax.swing.JLabel icon_user;
-    private javax.swing.JButton jButton1;
     private javax.swing.JPanel jPanel1;
-    private javax.swing.JLabel txt_login;
     private javax.swing.JTextField txt_password;
+    private javax.swing.JLabel txt_register;
     private javax.swing.JTextField txt_user;
     // End of variables declaration//GEN-END:variables
 }
